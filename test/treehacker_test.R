32e0a5ec067c99d1a988fc5f2e7dd1dfc9f8d359
Johanna Paijmans <paijmans.jla@gmail.com>
2025-07-11 10:03:28 +0100

    test installation CI

--- test/treehacker_test.R
+++ test/treehacker_test.R
@@ -15,9 +15,9 @@ if (dir.exists("./output_*")){
   dir.remove("./output_*")
 }
 
-# test with a simple set with full scaffolds
+# test with a simple command that it runs at all
 test_that("TREEhacker runs successfully",{
-  res <- system2("../TREEhacker_1.0_raxml_DNA-BIN_NsFilter.sh"," fastafiles.txt test 20 20 0.5 DNA",
+  res <- system2("../TREEhacker_1.0_raxml_DNA-BIN_NsFilter.sh"," fastafiles.txt test 20 20 0.5 BOTH",
           stdout=TRUE)
   # check if the output files are created
   expect_true(file.exists("fasta1_20_20s_wins.fasta"))
@@ -25,4 +25,30 @@ test_that("TREEhacker runs successfully",{
   expect_true(dir.exists("output_TREEhackerFiles_test/"))
   # check if the tree files are created in the output directory
   expect_true(file.exists("*bestTree"))
+  # remove output files
+  file.remove("*_wins.fasta")
+  file.remove("test*")
+  # remove output directory
+  unlink("output_TREEhackerFiles_test", recursive = TRUE)
 })
+
+# test with a simple set with full scaffolds
+test_that("TREEhacker filters windows properly",{
+  res <- system2("../TREEhacker_1.0_raxml_DNA-BIN_NsFilter.sh"," fastafiles.txt test 20 20 0.5 DNA",
+          stdout=TRUE)
+  # check if NO output files are created for scaffold_3_0-20 as it shouldnt pass 0.5 filter
+  expect_false(file.exists("output_TREEhackerFiles_test/scaffold_3_0-20.concat.RN.fasta.raxml.bestTree"))
+  file.remove("*_wins.fasta")
+  file.remove("test*")
+  # remove output directory
+  unlink("output_TREEhackerFiles_test", recursive = TRUE)
+
+  # but it should pass the 0.1 filter
+  res <- system2("../TREEhacker_1.0_raxml_DNA-BIN_NsFilter.sh"," fastafiles.txt test 20 20 0.1 DNA",
+          stdout=TRUE)
+  expect_true(file.exists("output_TREEhackerFiles_test/scaffold_3_0-20.concat.RN.fasta.raxml.bestTree"))
+  file.remove("*_wins.fasta")
+  file.remove("test*")
+  # remove output directory
+  unlink("output_TREEhackerFiles_test", recursive = TRUE)
+  })
