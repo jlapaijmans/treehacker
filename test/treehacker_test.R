3246caad0c9375bbc2eefe3ad34c153dd0bc470e
Johanna Paijmans <paijmans.jla@gmail.com>
2025-07-11 10:19:31 +0100

    test installation CI

--- test/treehacker_test.R
+++ test/treehacker_test.R
@@ -9,10 +9,10 @@ if(!require("ape", quietly = TRUE)) install.packages("ape")
 library(testthat)
 # remove the old test fasta file if it still exists
 if (file.exists("./*win*.fasta")){
-  file.remove("./*win*.fasta")
+  unlink("./*win*.fasta*")
 }
 if (dir.exists("./output_*")){
-  dir.remove("./output_*")
+  unlink("./output_*", recursive = TRUE)
 }
 
 # test with a simple command that it runs at all
@@ -24,31 +24,32 @@ test_that("TREEhacker runs successfully",{
   # check if the output directory is created
   expect_true(dir.exists("output_TREEhackerFiles_test/"))
   # check if the tree files are created in the output directory
-  expect_true(file.exists("*bestTree"))
+  expect_true(file.exists("output_TREEhackerFiles_test/scaffold_1_0-20.concat.RN.BIN.fasta.raxml.bestTree"))
+  expect_true(file.exists("output_TREEhackerFiles_test/scaffold_1_0-20.concat.RN.fasta.raxml.bestTree"))
   # remove output files
-  file.remove("*_wins.fasta")
-  file.remove("test*")
+  unlink("./*win*.fasta")
+  unlink("test*")
   # remove output directory
   unlink("output_TREEhackerFiles_test", recursive = TRUE)
 })
 
-# test with a simple set with full scaffolds
+# test if the filtering happens correctly (example scaffold has 5 Ns out of 20 positions)
 test_that("TREEhacker filters windows properly",{
   res <- system2("../TREEhacker_1.0_raxml_DNA-BIN_NsFilter.sh"," fastafiles.txt test 20 20 0.5 DNA",
           stdout=TRUE)
-  # check if NO output files are created for scaffold_3_0-20 as it shouldnt pass 0.5 filter
-  expect_false(file.exists("output_TREEhackerFiles_test/scaffold_3_0-20.concat.RN.fasta.raxml.bestTree"))
-  file.remove("*_wins.fasta")
-  file.remove("test*")
+  # check if output files are created for scaffold_3_0-20 as it should pass 0.5 filter
+  expect_true(file.exists("output_TREEhackerFiles_test/scaffold_3_0-20.concat.RN.fasta.raxml.bestTree"))
+  unlink("*_wins.fasta")
+  unlink("test*")
   # remove output directory
   unlink("output_TREEhackerFiles_test", recursive = TRUE)
 
-  # but it should pass the 0.1 filter
+  # but it should NOT pass the 0.1 filter
   res <- system2("../TREEhacker_1.0_raxml_DNA-BIN_NsFilter.sh"," fastafiles.txt test 20 20 0.1 DNA",
           stdout=TRUE)
-  expect_true(file.exists("output_TREEhackerFiles_test/scaffold_3_0-20.concat.RN.fasta.raxml.bestTree"))
-  file.remove("*_wins.fasta")
-  file.remove("test*")
+  expect_false(file.exists("output_TREEhackerFiles_test/scaffold_3_0-20.concat.RN.fasta.raxml.bestTree"))
+  unlink("*_wins.fasta")
+  unlink("test*")
   # remove output directory
   unlink("output_TREEhackerFiles_test", recursive = TRUE)
   })
