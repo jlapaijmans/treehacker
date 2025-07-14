728cb6520c497fa42d555645def6c392f58122c4
Johanna Paijmans <paijmans.jla@gmail.com>
2025-07-14 14:01:18 +0100

    test installation CI

--- test/treehacker_test.R
+++ test/treehacker_test.R
@@ -15,7 +15,7 @@ if (dir.exists("./output_*")){
 
 # test with a simple command that it runs at all
 test_that("TREEhacker runs successfully",{
-  res <- system2("../TREEhacker_1.0.sh"," fastafiles.txt test 20 20 0.5 BOTH",
+  res <- system2("../TREEhacker_1.0.sh","  --threads 1 --parallel 1 fastafiles.txt test 20 20 0.5 BOTH",
           stdout=TRUE)
   # check if the output files are created
   expect_true(file.exists("fasta1_20_20s_wins.fasta"))
@@ -33,7 +33,7 @@ test_that("TREEhacker runs successfully",{
 
 # test if the filtering happens correctly (example scaffold has 5 Ns out of 20 positions)
 test_that("TREEhacker filters windows properly",{
-  res <- system2("../TREEhacker_1.0.sh"," fastafiles.txt test 20 20 0.5 DNA",
+  res <- system2("../TREEhacker_1.0.sh","  --threads 1 --parallel 1 fastafiles.txt test 20 20 0.5 DNA",
           stdout=TRUE)
   # check if output files are created for scaffold_3_0-20 as it should pass 0.5 filter
   expect_true(file.exists("output_TREEhackerFiles_test/scaffold_3_0-20.concat.RN.fasta.raxml.bestTree"))
@@ -43,7 +43,7 @@ test_that("TREEhacker filters windows properly",{
   unlink("output_TREEhackerFiles_test", recursive = TRUE)
 
   # but it should NOT pass the 0.1 filter
-  res <- system2("../TREEhacker_1.0.sh"," fastafiles.txt test 20 20 0.1 DNA",
+  res <- system2("../TREEhacker_1.0.sh","  --threads 1 --parallel 1 fastafiles.txt test 20 20 0.1 DNA",
           stdout=TRUE)
   expect_false(file.exists("output_TREEhackerFiles_test/scaffold_3_0-20.concat.RN.fasta.raxml.bestTree"))
   unlink("*_wins.fasta*")
