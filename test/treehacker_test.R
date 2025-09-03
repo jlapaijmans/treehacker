3b629e459b88bdfc7979984b5cc4ff6fcdbaf011
Johanna Paijmans <paijmans.jla@gmail.com>
2025-09-03 10:05:15 +0100

    removing intermediate files as you go

--- test/treehacker_test.R
+++ test/treehacker_test.R
@@ -15,57 +15,77 @@ if (dir.exists("./output_*")){
 
 # test with a simple command that it runs at all
 test_that("TREEhacker runs successfully",{
-  res <- system2("../TREEhacker_1.0.sh"," --threads 1 --parallel 1 fastafiles.txt test 20 20 0.5 BOTH",
+  res <- system2("../TREEhacker.sh"," --threads 1 --parallel 1 fastafiles.txt test 20 20 0.5 BOTH",
           stdout=TRUE)
   # check if the output files are created
   expect_true(file.exists("fasta1_20_20s_wins.fasta"))
-  # check if the output directory is created
-  expect_true(dir.exists("output_TREEhackerFiles_test/"))
-  # check if the tree files are created in the output directory
-  expect_true(file.exists("output_TREEhackerFiles_test/scaffold_1_0-20.concat.RN.BIN.fasta.raxml.bestTree"))
-  expect_true(file.exists("output_TREEhackerFiles_test/scaffold_1_0-20.concat.RN.fasta.raxml.bestTree"))
+  # check if the master tree collection files are created
+  expect_true(file.exists("test_raxml_trees.txt"))
+  expect_true(file.exists("test_raxml_trees_BIN.txt"))
+  # check if the topology summary files are created
+  expect_true(file.exists("test_raxml_trees_TOPO.txt"))
+  expect_true(file.exists("test_raxml_trees_TOPO_BIN.txt"))
+  # check if master tree files have correct format (header + data)
+  trees_dna <- readLines("test_raxml_trees.txt")
+  trees_bin <- readLines("test_raxml_trees_BIN.txt")
+  expect_true(length(trees_dna) > 1)  # Should have header + trees
+  expect_true(length(trees_bin) > 1)  # Should have header + trees
+  expect_true(grepl("^window\ttree$", trees_dna[1]))  # Check header format
+  expect_true(grepl("^window\ttree$", trees_bin[1]))  # Check header format
+  expect_true(grepl("^scaffold_.*\t.*\\);$", trees_dna[2]))  # Check data format
+  expect_true(grepl("^scaffold_.*\t.*\\);$", trees_bin[2]))  # Check data format
+  # check that output directory was cleaned up (should NOT exist after successful run)
+  expect_false(dir.exists("output_TREEhackerFiles_test/"))
   # remove output files
   unlink("./*win*.fasta*")
   unlink("test*")
-  # remove output directory
-  unlink("output_TREEhackerFiles_test", recursive = TRUE)
 })
 
 # test with a simple command that it runs at all with zipped input
 test_that("TREEhacker runs successfully with zipped fasta.gz ",{
-  res <- system2("../TREEhacker_1.0.sh"," --threads 1 --parallel 1 fastafiles_gz.txt test 20 20 0.5 BOTH",
+  res <- system2("../TREEhacker.sh"," --threads 1 --parallel 1 fastafiles_gz.txt test 20 20 0.5 BOTH",
           stdout=TRUE)
   # check if the output files are created
   expect_true(file.exists("fasta1_20_20s_wins.fasta"))
-  # check if the output directory is created
-  expect_true(dir.exists("output_TREEhackerFiles_test/"))
-  # check if the tree files are created in the output directory
-  expect_true(file.exists("output_TREEhackerFiles_test/scaffold_1_0-20.concat.RN.BIN.fasta.raxml.bestTree"))
-  expect_true(file.exists("output_TREEhackerFiles_test/scaffold_1_0-20.concat.RN.fasta.raxml.bestTree"))
+  # check if the master tree collection files are created
+  expect_true(file.exists("test_raxml_trees.txt"))
+  expect_true(file.exists("test_raxml_trees_BIN.txt"))
+  # check if the topology summary files are created
+  expect_true(file.exists("test_raxml_trees_TOPO.txt"))
+  expect_true(file.exists("test_raxml_trees_TOPO_BIN.txt"))
+  # check that output directory was cleaned up (should NOT exist after successful run)
+  expect_false(dir.exists("output_TREEhackerFiles_test/"))
   # remove output files
   unlink("./*win*.fasta*")
   unlink("test*")
-  # remove output directory
-  unlink("output_TREEhackerFiles_test", recursive = TRUE)
 })
 
 # test if the filtering happens correctly (example scaffold has 5 Ns out of 20 positions)
 test_that("TREEhacker filters windows properly",{
-  res <- system2("../TREEhacker_1.0.sh","  --threads 1 --parallel 1 fastafiles.txt test 20 20 0.5 DNA",
+  res <- system2("../TREEhacker.sh","  --threads 1 --parallel 1 fastafiles.txt test 20 20 0.5 DNA",
           stdout=TRUE)
-  # check if output files are created for scaffold_3_0-20 as it should pass 0.5 filter
-  expect_true(file.exists("output_TREEhackerFiles_test/scaffold_3_0-20.concat.RN.fasta.raxml.bestTree"))
+  # check if master tree file is created and contains scaffold_3_0-20 (should pass 0.5 filter)
+  expect_true(file.exists("test_raxml_trees.txt"))
+  trees <- readLines("test_raxml_trees.txt")
+  expect_true(any(grepl("scaffold_3_0-20", trees)))
+  # check that output directory was cleaned up
+  expect_false(dir.exists("output_TREEhackerFiles_test/"))
   unlink("*_wins.fasta*")
   unlink("test*")
-  # remove output directory
-  unlink("output_TREEhackerFiles_test", recursive = TRUE)
 
   # but it should NOT pass the 0.1 filter
-  res <- system2("../TREEhacker_1.0.sh"," --threads 1 --parallel 1 fastafiles.txt test 20 20 0.1 DNA",
+  res <- system2("../TREEhacker.sh"," --threads 1 --parallel 1 fastafiles.txt test 20 20 0.1 DNA",
           stdout=TRUE)
-  expect_false(file.exists("output_TREEhackerFiles_test/scaffold_3_0-20.concat.RN.fasta.raxml.bestTree"))
+  # check if master tree file exists but should NOT contain scaffold_3_0-20
+  if(file.exists("test_raxml_trees.txt")) {
+    trees <- readLines("test_raxml_trees.txt")
+    expect_false(any(grepl("scaffold_3_0-20", trees)))
+  } else {
+    # If no trees passed the filter, that's also acceptable
+    expect_true(TRUE)
+  }
+  # check that output directory was cleaned up
+  expect_false(dir.exists("output_TREEhackerFiles_test/"))
   unlink("*_wins.fasta*")
   unlink("test*")
-  # remove output directory
-  unlink("output_TREEhackerFiles_test", recursive = TRUE)
   })
